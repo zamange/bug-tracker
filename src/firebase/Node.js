b8838d80c795e73f0092c165174141c2e3435860
Zamagcina Ngeyane <zamagcinangeyane6@gmail.com>
2025-02-03 17:42:16 +0200

    changed stack

--- src/firebase/Node.js
+++
@@ -1,7 +0,0 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("/home/zamagcina/bug-tracker-e606b-firebase-adminsdk-fbsvc-ab546ce6e8.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
